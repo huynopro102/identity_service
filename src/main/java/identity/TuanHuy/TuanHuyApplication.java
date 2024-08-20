a30ae17b01ccba35f012961b4e6ed26049d15523
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-08-20 14:50:00 +0700

    first commit

---
+++ src/main/java/identity/TuanHuy/TuanHuyApplication.java
@@ -0,0 +1,13 @@
+package identity.TuanHuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuanHuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuanHuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/identity/TuanHuy/TuanHuyApplicationTests.java
@@ -0,0 +1,13 @@
+package identity.TuanHuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuanHuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
