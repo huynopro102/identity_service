21b4b6ead47142d28fe11e7d13ab2f4d47390a79
tuan huy <100185063+huynopro102@users.noreply.github.com>
2024-10-23 00:05:53 +0700

    alert class abstract hello jenkin lan 5

--- src/main/java/identity/TuanHuy/abstract_class/main.java
+++ src/main/java/identity/TuanHuy/abstract_class/main.java
@@ -9,5 +9,6 @@ public class main {
         System.out.println("hello jenkin lan 2");
         System.out.println("hello jenkin lan 3");
         System.out.println("hello jenkin lan 4");
+        System.out.println("hello jenkin lan 5");
     }
 }
