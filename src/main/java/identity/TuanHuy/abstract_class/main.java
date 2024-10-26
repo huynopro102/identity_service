414864899b2fff2621e052a01471ce5eb6147d0f
x thang <x_thang@example.com>
2024-10-26 10:07:00 +0700

    jenkin lan 15

--- src/main/java/identity/TuanHuy/abstract_class/main.java
+++ src/main/java/identity/TuanHuy/abstract_class/main.java
@@ -19,5 +19,6 @@ public class main {
         System.out.println("hello jenkin lan 12");
         System.out.println("hello jenkin lan 13");
         System.out.println("hello jenkin lan 14");
+        System.out.println("hello jenkin lan 15");
     }
 }
