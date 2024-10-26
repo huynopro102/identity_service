03979f286779b02dafb4d9249bf8bd064b55a770
x thang <x_thang@example.com>
2024-10-26 12:49:55 +0700

    jenkin lan 16

--- src/main/java/identity/TuanHuy/abstract_class/main.java
+++ src/main/java/identity/TuanHuy/abstract_class/main.java
@@ -20,5 +20,6 @@ public class main {
         System.out.println("hello jenkin lan 13");
         System.out.println("hello jenkin lan 14");
         System.out.println("hello jenkin lan 15");
+        System.out.println("hello jenkin lan 16");
     }
 }
