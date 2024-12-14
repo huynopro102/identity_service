ec0096426d17b87e757aadff7f9de73df64675ae
x thang <x_thang@example.com>
2024-12-14 20:31:47 +0700

    add file TuanHuy-0.0.1-SNAPSHOT.jar

--- src/main/java/identity/TuanHuy/TuanHuyApplication.java
+++ src/main/java/identity/TuanHuy/TuanHuyApplication.java
@@ -1,13 +1,17 @@
 package identity.TuanHuy;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-//@EntityScan(basePackages = "identity.TuanHuy.UI.entity")
 public class TuanHuyApplication {
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+		System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+		System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 		SpringApplication.run(TuanHuyApplication.class, args);
 		System.out.println("hello world");
 	}
