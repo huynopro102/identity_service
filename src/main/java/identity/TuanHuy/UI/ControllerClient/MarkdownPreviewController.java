b60d62344b970fcd3d3f7fc2a59026581ce93e6a
Nguyen Tuan Huy <huynopro102@gmail.com>
2025-01-15 13:05:58 +0700

    add ui markdown_preview

---
+++ src/main/java/identity/TuanHuy/UI/ControllerClient/MarkdownPreviewController.java
@@ -0,0 +1,17 @@
+package identity.TuanHuy.UI.ControllerClient;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ui")
+public class MarkdownPreviewController {
+
+    @GetMapping("/markdown_preview")
+    public String markdown_preview(Model model){
+        model.addAttribute("content" , "/Client/markdown_preview/index");
+        return "fragments/baseClient";
+    }
+}
